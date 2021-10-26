f281383f61b6a76d44be66af0d2b712d04e363ca
Henry <henri.oliveira.93@gmail.com>
2021-10-25 21:27:38 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato("Felix","Preto",10);
+        System.out.println(gato);
+    //    int a = 2;
+    //    int b = 5;
+    //    int soma = a + b;
+    //    System.out.println("Hello world"+soma);
+
+    }
+}
